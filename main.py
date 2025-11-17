5675988bbdb44accc5194251ae9e809ac7322dd2
Kaho Legradi <legradi.kaho+github@gmail.com>
2025-11-17 15:04:27 +0000

    change

--- main.py
+++ main.py
@@ -7,36 +7,37 @@ app = Flask(__name__)
 
 # don't forget to source data.env file before running
 app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
+google_maps_api_key = os.environ.get("GOOGLE_MAPS_API_KEY")
 Bootstrap5(app)
 
 # all Flask routes below
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", google_maps_api_key=google_maps_api_key)
 
 @app.route("/search")
 def search():
-    return render_template("listing.html")
+    return render_template("listing.html", google_maps_api_key=google_maps_api_key)
 
 @app.route("/explore")
 def explore():
-    return render_template("listing.html")
+    return render_template("listing.html", google_maps_api_key=google_maps_api_key)
 
 @app.route("/listing")
 def listing():
-    return render_template("listing.html")
+    return render_template("listing.html", google_maps_api_key=google_maps_api_key)
 
 @app.route("/single_listing")
 def single_listing():
-    return render_template("listing.html")
+    return render_template("single_listing.html", google_maps_api_key=google_maps_api_key)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", google_maps_api_key=google_maps_api_key)
 
 @app.route("/auth_login")
 def auth_login():
-    return render_template("auth_login.html")
+    return render_template("auth_login.html", google_maps_api_key=google_maps_api_key)
 
 
 if __name__ == '__main__':
